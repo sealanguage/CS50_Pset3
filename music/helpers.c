5b908fa9a75302f7530f57f801f0afd042e930d2
Stephanie Shea <stephcshea@gmail.com>
2018-04-23 02:52:00 +0000

    added for comments to completed  music

--- music/helpers.c
+++ music/helpers.c
@@ -27,39 +27,40 @@ int duration(string fraction)
 //to make a note shartp mult orig freq by 2^1.12 for flat, divide
 int frequency(string note)
 {
-    // inputs a note, outputs in integer e.g "A#4": parse string: figure out note and octave; calculate the hertz frequency of not in octave; return hertz (A440Hz) value as integer
+    //inputs a note, outputs in integer e.g "A#4": parse string: figure out note and octave; calculate the hertz frequency of not in octave; return hertz (A440Hz) value as integer
     //semitones move up: mult frequency by 2^1/12 and move down divide frequency by 2^1/12
-    char letter = note[0];
-    char octave;
-    char accidental;
-    float frequency = 0.;
+    char letter = note[0]; //extracts the 'letter' of the note
+    char octave; //declares what will hold octave number
+    char accidental; //declares what will hold # or b
+    float frequency = 0.; //declared frequency
 
-    if (strlen(note)  == 3)
+    if (strlen(note)  == 3) //if note has 3 characters
     {
-        octave = atoi(&note[2]);
+        octave = atoi(&note[2]); //note[2] converted to int = octave
     }
 
     else
     {
-        octave = atoi(&note[1]);
+        octave = atoi(&note[1]); //other note[1] converted to int = octave
     }
 
-
-    if (strlen(note)  == 3)
+    if (strlen(note)  == 3) //if note has 3 characters
     {
-        accidental = note[1];
+        accidental = note[1]; //note[1] = accidental
     }
 
     else
     {
-        accidental = 0;
+        accidental = 0; //not accidental
     }
 
-    float base = 0.0;
-    float powerof = (1.0 / 12.0);
+    float base = 0.0; //declares base for frequency
+    float powerof = (1.0 / 12.0); //defines incrementor for accidentals
 
     float powwow = (pow(2, (powerof)));
+    //equation to determine value to add to base if accidentals exist
 
+    //iterate thru letter of string to determine base value
     if (letter == 'C')
     {
         base = 16.35 * (pow(2, octave));
@@ -99,22 +100,23 @@ int frequency(string note)
         printf("need another letter");
     }
 
+    //check for accidentals
     if (accidental == 0)
     {
-        frequency = (base);
+        frequency = (base); //if no accidentals base remaims the same
     }
 
     else if (accidental == '#')
     {
-        frequency = (base * powwow);
+        frequency = (base * powwow);  //if # perform equation
     }
 
     else if (accidental == 'b')
     {
-        frequency = (base / powwow);
+        frequency = (base / powwow); //if # perform equation
     }
 
-    return round(frequency);
+    return round(frequency); //round frequency to whole number
 
 }
 
